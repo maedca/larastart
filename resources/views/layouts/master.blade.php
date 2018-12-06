4214c664ccefc4edc95be58866bee0aa16d00f9b
Manuel Camacho <maedca@gmail.com>
2018-12-06 19:21:28 -0400

    change font color and logout implementation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -70,7 +70,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
 
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <i class="nav-icon fas fa-tachometer-alt blue"></i>
                             <p>
                                 Dashboard
                                 {{--<span class="right badge badge-danger">New</span>--}}
@@ -80,7 +80,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                     <li class="nav-item has-treeview ">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-cogs"></i>
+                            <i class="nav-icon fas fa-cogs green"></i>
                             <p>
                                 Management
                                 <i class="right fas fa-angle-left"></i>
@@ -103,7 +103,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     </li>
                     <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
-                            <i class="nav-icon fas fa-user"></i>
+                            <i class="nav-icon fas fa-user orange"></i>
                             <p>
                                 Profile
                                 {{--<span class="right badge badge-danger">New</span>--}}
@@ -111,13 +111,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-power-off"></i>
-                            <p>
-                                Logout
+                        {{--<a href="#" class="nav-link">--}}
+                            {{--<i class="nav-icon fas fa-power-off"></i>--}}
+                            {{--<p>--}}
+                                {{--Logout--}}
                                 {{--<span class="right badge badge-danger">New</span>--}}
-                            </p>
+                            {{--</p>--}}
+                        {{--</a>--}}
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            <i class="nav-icon fas fa-power-off red"></i>
+                                <p>{{ __('Logout') }}</p>
+
                         </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
 
                 </ul>
