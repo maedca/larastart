a1c296ced0602e033e20b3c8c6c76cefe386fd97
Manuel Camacho <maedca@gmail.com>
2018-12-06 21:35:32 -0400

    create user table and adding modal to show create user form

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,12 +1,62 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Users Component</div>
+        <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">User Table</h3>
 
-                    <div class="card-body">
-                        I'm an example component.
+                    <div class="card-tools">
+                        <!--<div class="input-group input-group-sm" style="width: 150px;">-->
+                            <!--<input type="text" name="table_search" class="form-control float-right" placeholder="Search">-->
+
+                            <!--<div class="input-group-append">-->
+                                <!--<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>-->
+                            <!--</div>-->
+                        <!--</div>-->
+                        <button class="btn btn-success " data-toggle="modal" data-target="#addNew">Add New <i class="fas fa-user-plus"></i></button>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-hover">
+                        <tbody><tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Type</th>
+                            <th>Modify</th>
+                        </tr>
+                        <tr>
+                            <td>183</td>
+                            <td>John Doe</td>
+                            <td>11-7-2014</td>
+                            <td><span class="tag tag-success">Approved</span></td>
+                            <td><a href=""><i class="fas fa-user-edit blue"></i></a> | <a href=""><i class="fas fa-user-times red"></i></a></td>
+                        </tr>
+
+                        </tbody></table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+        </div>
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNewLabel">Add New</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Create</button>
                     </div>
                 </div>
             </div>
